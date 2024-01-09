a8e4ebaa467173d9b235acbef472dad489814dfb
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-09 13:07:14 +0700

    finish challenge task api native

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type responseSucces struct {
-	Status  string `json:"status"`
-	Message string `json:"message"`
+	Status  string      `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"Data,omitempty"`
 }
 
 type responseError struct {
@@ -17,12 +20,42 @@ type responseError struct {
 	Message string `json:"message"`
 }
 
-type todo struct {
-	id   int
-	task string
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      bool   `json:"status"`
 }
 
-var todos []todo
+var tasks []Task
+
+func RequestMethodGet(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			json.NewEncoder(w).Encode(responseError{
+				Status:  "error",
+				Message: "Method Not Allowed",
+			})
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func RequestMethodDelete(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "DELETE" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			json.NewEncoder(w).Encode(responseError{
+				Status:  "error",
+				Message: "Method Not Allowed",
+			})
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -30,10 +63,14 @@ func main() {
 		w.Write([]byte("Hello World!"))
 	})
 
-	http.HandleFunc("/task", getTask)
+	http.Handle("/task", RequestMethodGet(http.HandlerFunc(getTask)))
+	http.Handle("/task/", RequestMethodGet(http.HandlerFunc(getTaskById)))
+
 	http.HandleFunc("/task/add", addTask)
 	http.HandleFunc("/task/update", updateTask)
-	http.HandleFunc("/task/delete", deleteTask)
+
+	http.Handle("/task/delete", RequestMethodDelete(http.HandlerFunc(deleteTask)))
+	http.Handle("/task/delete/all", RequestMethodDelete(http.HandlerFunc(deleteAllTask)))
 
 	fmt.Println("Server Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -46,33 +83,87 @@ func addTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(map[string]string{
-		"status":  "success",
-		"message": "Task added",
-	})
+	var newTask Task
+	err := json.NewDecoder(r.Body).Decode(&newTask)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(responseError{
+			Status:  "Error",
+			Message: "Failed to decode json",
+		})
+		return
+	}
+
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err = json.NewEncoder(w).Encode(responseSucces{
+		Status:  "OK",
+		Message: "Sucesfully Added New Task",
+		Data:    tasks,
+	}); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
 
+func getTaskById(w http.ResponseWriter, r *http.Request) {
+	param := strings.TrimPrefix(r.URL.Path, "/task/")
+	id, err := strconv.Atoi(param)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(responseError{
+			Status:  "error",
+			Message: "id needs to be number",
+		})
+		return
+	}
+	for _, task := range tasks {
+		if task.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(responseSucces{
+				Status:  "OK",
+				Message: "Sucesfully get data",
+				Data:    task,
+			})
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(responseSucces{
+		Status:  "error",
+		Message: "task not found",
+	})
+}
+
 func getTask(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(responseSucces{
+		Status:  "OK",
+		Message: "Data",
+		Data:    tasks,
+	}); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func deleteAllTask(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "DELETE" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(w).Encode(responseError{Message: "Metod Not Allowed", Status: "error"})
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(responseSucces{
-		Status:  "succes",
-		Message: "Data",
+	tasks = []Task{}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(responseSucces{
+		Status:  "Succes",
+		Message: "All Task Deleted",
+		Data:    tasks,
 	})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 }
 
 func deleteTask(w http.ResponseWriter, r *http.Request) {
@@ -82,31 +173,72 @@ func deleteTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(responseSucces{
-		Status:  "succes",
-		Message: "Task Deleted",
-	})
+	var newTask Task
+	err := json.NewDecoder(r.Body).Decode(&newTask)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(responseError{
+			Status:  "Error",
+			Message: "Failed to decode json",
+		})
 		return
 	}
+
+	for index, task := range tasks {
+		if task.ID == newTask.ID {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+		}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(responseSucces{
+			Status:  "Succes",
+			Message: "Task Deleted",
+		})
+
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(responseError{
+		Status:  "error",
+		Message: "Task Not Found",
+	})
 }
 
 func updateTask(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "UPDATE" {
+	if r.Method != "PUT" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(w).Encode(responseError{Message: "Metod Not Allowed", Status: "error"})
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(responseSucces{
-		Status:  "succes",
-		Message: "Task Updated",
-	})
+	var newTask Task
+	err := json.NewDecoder(r.Body).Decode(&newTask)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(responseError{
+			Status:  "Error",
+			Message: "Failed to decode json",
+		})
 		return
 	}
+
+	for index, task := range tasks {
+		if task.ID == newTask.ID {
+			tasks[index].Title = newTask.Title
+			tasks[index].Description = newTask.Description
+			tasks[index].Status = newTask.Status
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(responseSucces{
+				Status:  "success",
+				Message: "Task Updated",
+				Data:    tasks,
+			})
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(responseSucces{
+		Status:  "error",
+		Message: "Task Not Found",
+	})
 }
