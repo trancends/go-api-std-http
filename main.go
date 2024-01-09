ce2b1a279dd51bcf7eba9acd77a53bb7a34bec85
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-09 09:56:23 +0700

    http init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Ok, starting..."))
+	})
+
+	fmt.Println("Server Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Hello")
+}
